fba537fc3ba32cb4932b5bc0f84b787d145124e8
PrageshShrestha <82960735+PrageshShrestha@users.noreply.github.com>
2025-05-30 21:05:05 +0545

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import ssl
@@ -25,24 +24,4 @@ async def init_db():
         print("Database initialized successfully.")
     except Exception as e:
         print(f"Database initialization failed: {e}")
-=======
-# database.py
-import os
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
-from base import Base  # Import Base from base.py
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:helloworld@localhost/uvicorn2")
-
-engine = create_async_engine(DATABASE_URL, echo=True)
-
-# Asynchronous session maker
-async_session_maker = sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
-)
-
-# Utility function to get DB session
-async def get_db():
-    async with async_session_maker() as session:
-        yield session
->>>>>>> 818a14e1226f981c3d9864e53ac84e04fd6561e1
